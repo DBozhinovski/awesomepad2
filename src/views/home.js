de04961a57910f0e973caf30e2c2c6154db6557e
Darko Bozhinovski <dbozhinovski@gmail.com>
2014-08-03 22:34:25 +0200

    Adds editor partial

--- src/views/home.js
+++ src/views/home.js
@@ -5,10 +5,14 @@ var Ractive = require('ractive');
 var fs = require('fs');
 
 var template = fs.readFileSync(__dirname + '/templates/index.ractive');
+var editor = fs.readFileSync(__dirname + '/templates/partials/editor.ractive');
 
 var Home = Ractive.extend({
   el: 'body',
-  template: (template.toString())
+  template: (template.toString()),
+  partials: {
+    editor: (editor.toString())
+  }
 });
 
 module.exports = Home;
