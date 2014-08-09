f33219d7ad91e9430a0d6ba006dadcd2248d56f0
Darko Bozhinovski <dbozhinovski@gmail.com>
2014-08-09 21:18:05 +0200

    Adds document model

---
+++ src/models/document.js
@@ -0,0 +1,11 @@
+/* jshint node:true, eqeqeq:true */
+"use strict";
+
+var Backbone = require('backbone');
+
+var Document = Backbone.Model.extend({
+  // should go to pouch DB
+  initalize: function(){
+
+  }
+});
