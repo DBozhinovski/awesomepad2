fad36245e6c9f6b3352fb97b9807e05fd1948570
Darko Bozhinovski <dbozhinovski@gmail.com>
2014-08-16 18:40:20 +0200

    Adds save

--- src/views/home.js
+++ src/views/home.js
@@ -2,6 +2,7 @@
 "use strict";
 
 var Ractive = require('ractive');
+require('ractive-adaptors-backbone');
 var fs = require('fs');
 
 var template = fs.readFileSync(__dirname + '/templates/index.ractive');
@@ -13,15 +14,20 @@ var browse = fs.readFileSync(__dirname + '/templates/partials/browse.ractive');
 var Home = Ractive.extend({
   el: 'body',
   template: (template.toString()),
+  adapt: ['Backbone'],
   partials: {
     editor: (editor.toString()),
     browse: (browse.toString())
   },
   init: function(){
     this.on('open', this.open);
+    this.on('save', this.save);
   },
   open: function(){
     console.log('opening file');
+  },
+  save: function(){
+    console.log(this.data);
   }
 });
 
