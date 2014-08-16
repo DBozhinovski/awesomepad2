690b0bca4a3824d965fdcd85340caf9a221dc0d1
Darko Bozhinovski <dbozhinovski@gmail.com>
2014-08-16 19:06:06 +0200

    Comment cleanup

--- src/views/home.js
+++ src/views/home.js
@@ -10,8 +10,6 @@ var template = fs.readFileSync(__dirname + '/templates/index.ractive');
 var editor = fs.readFileSync(__dirname + '/templates/partials/editor.ractive');
 var browse = fs.readFileSync(__dirname + '/templates/partials/browse.ractive');
 
-// Needs backbone adaptor
-
 var Home = Ractive.extend({
   el: 'body',
   template: (template.toString()),
