b87119c09063cab97f6517e8ab47d03a12376a35
Darko Bozhinovski <dbozhinovski@gmail.com>
2014-08-24 21:15:05 +0200

    Adds gulp concat for the stylus task

--- gulp/tasks/stylus.js
+++ gulp/tasks/stylus.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
 var stylus = require('gulp-stylus');
 
-gulp.task('stylus', function(){
 
+gulp.task('stylus', function(){
+  gulp.src('./src/styles/**/*.styl')
+    .pipe(stylus())
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest('./public/css'));
 });
