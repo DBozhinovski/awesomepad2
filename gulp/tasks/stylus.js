080a9144ce37de6c495dd0444cbab2dac8d83ecc
Darko Bozhinovski <dbozhinovski@gmail.com>
2014-08-24 21:17:08 +0200

    Adds nib in the task pipeline

--- gulp/tasks/stylus.js
+++ gulp/tasks/stylus.js
@@ -1,11 +1,12 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var stylus = require('gulp-stylus');
+var nib = require('nib');
 
 
 gulp.task('stylus', function(){
   gulp.src('./src/styles/**/*.styl')
-    .pipe(stylus())
+    .pipe(stylus({use: [nib()]}))
     .pipe(concat('styles.css'))
     .pipe(gulp.dest('./public/css'));
 });
