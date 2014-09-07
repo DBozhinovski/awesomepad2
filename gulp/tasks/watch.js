8b1b92844bd4821da9ce7f84bbccc4caf94fa8ae
Darko Bozhinovski <dbozhinovski@gmail.com>
2014-09-07 21:08:16 +0200

    Initial pure grid setup

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -6,4 +6,6 @@ var gulp = require('gulp');
 gulp.task('watch', ['setWatch', 'browserify'], function(){
   gulp.watch('./src/styles/**', ['stylus']);
   gulp.watch('./src/assets/**', ['static']);
+  gulp.watch('./src/**/*.js', ['browserify']);
+  gulp.watch('./src/**/*.ractive', ['browserify']);
 });
