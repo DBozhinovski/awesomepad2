3aac3779d0b50fa4f7615a34a45aee77226c5f88
Darko Bozhinovski <dbozhinovski@gmail.com>
2014-09-13 21:09:49 +0200

    Updates browserify task, to work with the newest version of browserify and watchify

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -1,49 +1,34 @@
 'use strict';
 
-var browserify   = require('browserify');
-// var watchify     = require('watchify');
+var browserify = require('browserify');
+var watchify = require('watchify');
 var bundleLogger = require('../util/bundleLogger');
-var gulp         = require('gulp');
+var gulp = require('gulp');
 var handleErrors = require('../util/handleErrors');
-var source       = require('vinyl-source-stream');
+var source = require('vinyl-source-stream');
 
 gulp.task('browserify', function() {
 
-	// var bundleMethod = global.isWatching ? watchify : browserify;
-	var bundleMethod = browserify;
-
-	var bundler = bundleMethod({
-		// Specify the entry point of your app
-		entries: ['./src/app.js'],
-		// Add file extentions to make optional in your requires
+	var bundler = watchify(browserify({
+		entries: './src/app.js',
 		extensions: ['.js', '.ractive'],
-		debug: true
-	});
+		debug: true,
+		cache: {},
+		pacakgeCache: {},
+		fullPaths: true
+	}));
 
-	var bundle = function() {
-		// Log when bundling starts
+	bundler.on('update', rebundle);
+
+	function rebundle(){
 		bundleLogger.start();
 
-		return bundler
-			// Enable source maps!
-			.bundle()
-			// Report compile errors
+		return bundler.bundle()
 			.on('error', handleErrors)
-			// Use vinyl-source-stream to make the
-			// stream gulp compatible. Specifiy the
-			// desired output filename here.
 			.pipe(source('app.js'))
-			// Specify the output destination
-			.pipe(gulp.dest('./public/js/'))
-			// Log when bundling completes!
+			.pipe(gulp.dest('./public/javascripts/'))
 			.on('end', bundleLogger.end);
-	};
-
-	// smell?
-	if(global.isWatching) {
-		// Rebundle with watchify on changes.
-		bundler.on('update', bundle);
 	}
 
-	return bundle();
+	return rebundle();
 });
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,9 +3,7 @@
 var gulp = require('gulp');
 
 // Watch and rerun stylus
-gulp.task('watch', ['setWatch', 'browserify'], function(){
+gulp.task('watch', ['browserify'], function(){
   gulp.watch('./src/styles/**', ['stylus']);
   gulp.watch('./src/assets/**', ['static']);
-  gulp.watch('./src/**/*.js', ['browserify']);
-  gulp.watch('./src/**/*.ractive', ['browserify']);
 });
