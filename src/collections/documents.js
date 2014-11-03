f8ebca9428edd3637a74a1b3e484db387afd02d7
Darko Bozhinovski <dbozhinovski@gmail.com>
2014-11-03 20:28:59 +0100

    Moves dependencies from bower to npm

--- src/collections/documents.js
+++ src/collections/documents.js
@@ -2,35 +2,9 @@
 
 var Backbone = require('backbone');
 var Model = require('../models/document');
-var PouchDB = require('pouchdb');
-var BackbonePouch = require('backbone-pouch');
-var _ = require('underscore');
 
 var Documents = Backbone.Collection.extend({
-  model: Model,
-  sync: BackbonePouch.sync({
-    db: new PouchDB('docs'),
-    fetch: 'query',
-    options: {
-      query: {
-        include_docs: true,
-        fun: {
-          /* jshint ignore:start */
-          // Since it's a scope thing, this isn't undefined;
-          // However, jshint still hates it, and rightfully so.
-          // So, I'll just ignore it for now :)
-          map: function(doc){
-            emit(doc.position, null);
-          }
-          /* jshint ignore:end */
-        },
-        limit: 10
-      }
-    },
-    parse: function(result) {
-      return _.pluck(result.rows, 'doc');
-    }
-  })
+  model: Model
 });
 
 module.exports = Documents;
--- src/models/document.js
+++ src/models/document.js
@@ -1,14 +1,8 @@
 'use strict';
 
 var Backbone = require('backbone');
-var PouchDB = require('pouchdb');
-var BackbonePouch = require('backbone-pouch');
 
 var Document = Backbone.Model.extend({
-  idAttribute: '_id',
-  sync: BackbonePouch.sync({
-    db: new PouchDB('docs'),
-  })
 });
 
 module.exports = Document;
