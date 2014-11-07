630ce0639188d383c694da478bcb230c554b4ec8
Darko Bozhinovski <dbozhinovski@gmail.com>
2014-11-07 22:24:58 +0100

    Chages stylus task to fix the gulp fuckup

--- gulp/tasks/stylus.js
+++ gulp/tasks/stylus.js
@@ -8,7 +8,10 @@ var nib = require('nib');
 // Compile and concat everything from app/styles to public/stylesheets/app.css
 gulp.task('stylus', function(){
   return gulp.src('./src/styles/**/*.styl')
-    .pipe(stylus({use: [nib()]}))
-    .pipe(concat('styles.css'))
+    .pipe(concat('styles'))
+    .pipe(stylus({
+      use: [ nib() ],
+      compress: true
+    }))
     .pipe(gulp.dest('./public/css/'));
 });
