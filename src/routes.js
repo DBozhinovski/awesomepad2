471276f1ca134f794d673eeef9e3a0471c6d9bf4
Darko Bozhinovski <dbozhinovski@gmail.com>
2014-11-10 00:05:53 +0100

    Removes manual route setting in routes

--- src/routes.js
+++ src/routes.js
@@ -22,17 +22,14 @@ var Router = Backbone.Router.extend({
 
   home: function(){
     this.homeView.set('route', 'browse');
-    // this.homeView.set({browse: true, edit: false, open: false});
   },
 
   new: function(){
     this.homeView.set({route: 'edit', doc: new Document()});
-    // this.homeView.set({browse: false, edit: true, open: false, doc: new Document() });
   },
 
   open: function(){
     this.homeView.set('route', 'open');
-    // this.homeView.set({browse: false, edit: false, open: true});
   },
   show: function(id){
     // Should render the doc
